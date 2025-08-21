9ee90b5f46355ce4d59d7f5cf01cd31da3142c95
kishlay <kishlay141@gmail.com>
2025-08-22 03:59:37 +0530

    feat: update footer to show full date with name and student number

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,13 @@
 const Footer = () => {
-  const year = new Date().getFullYear();
+  const dateStr = new Date().toLocaleDateString("en-AU", {
+    day: "2-digit",
+    month: "short",
+    year: "numeric",
+  });
 
   return (
     <footer className="fixed bottom-0 left-0 right-0 p-4 text-center border-t border-gray-800 dark:border-gray-200 bg-white dark:bg-gray-900 text-black dark:text-white">
-      © {year} Rohan Khurana, 21358295
+      © {dateStr} Rohan Khurana, 21358295
     </footer>
   );
 };
